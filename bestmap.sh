8d64148615089ecc0ec0225eb0100a4c9ffba068
Christopher Abad <aempirei@gmail.com>
2012-05-03 07:52:18 -0700

    bestmap code

---
+++ bestmap.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+source etc/config
+
+if [ ! -f "$1" ] || [ ! -f "$2" ]; then
+	echo
+	echo usage: $0 filelist1 filelist2
+	echo
+	echo filelist1 and filelist2 should be lists of relative paths to files
+	echo for comparison and mapping. each list should be of identical size,
+	echo but this is not necessary.
+	echo
+	exit
+fi
+
+sz=`wc -l < "$1"`
+sig=`basename "$1"`'+'`basename "$2"`
+
+weightfile="$sig.weights"
+matchfile="$sig.matching"
+
+rm -f "$weightfile"
+
+i=1
+
+function comp () { 
+
+	if [ ! -f "$1" ]; then
+		echo "error: file '$1' doesn't exist"
+		exit
+	fi
+
+	if [ ! -f "$2" ]; then
+		echo "error: file '$2' doesn't exist"
+		exit
+	fi
+
+	if [ $use_minimal -ne 0 ]; then
+		cmd="diff --minimal \"$1\" \"$2\""
+	else
+		cmd="diff \"$1\" \"$2\""
+	fi
+
+	if [ $use_gzip -ne 0 ]; then
+		cmd="$cmd | gzip -c1"
+	fi
+
+	cmd="$cmd | wc -c"
+
+	weight=`bash -c "$cmd"`
+
+	echo "$weight,${1//,/\\,},${2//,/\\,}" >> "$weightfile"
+}
+
+while read left; do
+
+	echo "$sig: $i/$sz ($((100*i/sz))%)"
+	
+	while read right; do
+		comp "$left" "$right"
+	done < "$2"
+
+	let i=$i+1
+done < "$1"
+
+echo performing bipartite matching
+
+./bipartite.pl < "$weightfile" > "$matchfile"
+
+cat "$matchfile"
+
+echo match file saved to "$matchfile"
---
+++ bipartite.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Algorithm::Munkres;
+
+my $lf = {};
+my $rf = {};
+
+my $lfr = {};
+my $rfr = {};
+
+my $im = [];
+my $om = [];
+
+my $lnext = 0;
+my $rnext = 0;
+
+my $maxweight = 0;
+
+while(my $line = <STDIN>) {
+
+	chomp $line;
+
+	my ( $weight, $l, $r ) = split(/(?<!\\),/, $line);
+
+	$l =~ s/\\,/,/g;
+	$r =~ s/\\,/,/g;
+
+	unless(exists $lfr->{$l}) {
+		$lfr->{$l} = $lnext;
+		$lf->{$lnext} = $l;
+		$im->[$lnext] = [];
+		$lnext++;
+	}
+
+	unless(exists $rfr->{$r}) {
+		$rfr->{$r} = $rnext;
+		$rf->{$rnext} = $r;
+		$rnext++;
+	}
+
+	my $li = $lfr->{$l};
+	my $ri = $rfr->{$r};
+
+	$im->[$li]->[$ri] = $weight;
+
+	$maxweight = $weight if($weight > $maxweight);
+}
+
+foreach my $row (@$im) {
+	foreach my $item (@$row) {
+		$item = $maxweight unless(defined $item);
+		#$item = $maxweight - $item;
+	}
+}
+
+assign($im, $om);
+
+foreach my $li (0..$#$om) {
+	my $ri = $om->[$li];
+	my $l = $lf->{$li};
+	my $r = $rf->{$ri};
+
+	$l =~ s/"/\\"/g;
+	$r =~ s/"/\\"/g;
+
+	print "\"$l\" => \"$r\"\n";
+}
